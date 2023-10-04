6c371c28bf812c5d99edf09ce2bc4fb10989bdbb
Daniils Petrovs <daniils@platogo.com>
2023-10-04 22:55:27 +0200

    Fix rss item preparation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,20 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+	const items = posts.map((post) => ({
+		title: post.data.title,
+		pubDate: post.data.pubDate,
+		description: post.data.description,
+		customData: post.data.customData,
+		// Compute RSS link from post `slug`
+		// This example assumes all posts are rendered as `/blog/[slug]` routes
+		link: `/blog/${post.slug}/`,
+	}));
+
 	return rss({
+		items,
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
 	});
 }
