159511bc83ec911cc30687ca89dc964f210354a2
Daniils Petrovs <daniils@platogo.com>
2023-10-28 23:39:31 +0200

    Implement initial photography page

---
+++ src/components/PhotoCarousel.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Carousel from 'svelte-carousel';
+
+	import { onMount } from 'svelte';
+
+	let isClient = false;
+
+	onMount(() => {
+		isClient = true;
+	});
+</script>
+
+{#if isClient}
+	<Carousel particlesToShow={3} particlesToScroll={1} dots={false}>
+		<slot />
+	</Carousel>
+{:else}
+	<slot />
+{/if}
