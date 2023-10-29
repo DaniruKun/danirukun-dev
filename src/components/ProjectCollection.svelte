b694c56481b6ba9fdf11db4ec6428f500d0476ec
Daniils Petrovs <daniils@platogo.com>
2023-10-29 03:13:23 +0100

    Improve mobile padding

--- src/components/ProjectCollection.svelte
+++ src/components/ProjectCollection.svelte
@@ -5,7 +5,7 @@
 	export let projects: Project[];
 </script>
 
-<section class="grid grid-cols-1 gap-4 bg-background md:grid-cols-2">
+<section class="grid grid-cols-1 gap-4 bg-background md:grid-cols-2 px-2">
 	{#each projects as project}
 		<ProjectEntry {...project} />
 	{/each}
