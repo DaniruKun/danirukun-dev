1a037adc666e642a396151f748419347a873e898
Daniils Petrovs <daniils@platogo.com>
2023-10-30 00:28:54 +0100

    Fix ProjectEntry visual bug

--- src/components/ProjectEntry.svelte
+++ src/components/ProjectEntry.svelte
@@ -12,7 +12,7 @@
 			class="h-full space-y-4 rounded-sm bg-primary p-4 text-primary-foreground drop-shadow-sm transition-all hover:-translate-y-2 hover:drop-shadow-xl"
 		>
 			<h2
-				class="scroll-m-20 rounded-sm border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+				class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
 			>
 				{title}
 			</h2>
@@ -25,7 +25,7 @@
 		class="h-full space-y-4 rounded-sm bg-primary p-4 text-primary-foreground drop-shadow-sm transition-all hover:-translate-y-2 hover:drop-shadow-xl"
 	>
 		<h2
-			class="scroll-m-20 rounded-sm border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
 		>
 			{title}
 		</h2>
